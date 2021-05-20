df4b75af93f6483194161ad051baa87912db364f
Praw <56972989+prawidayumia@users.noreply.github.com>
2021-05-21 04:48:32 +0700

    Add files via upload

---
+++ CharacterControl.cs
@@ -0,0 +1,11 @@
+﻿public class CharacterControl : MonoBehaviour
+{
+    public int speed = 3;
+
+    void Update()
+    {
+        transform.Translate(new Vector3((Input.GetAxis("Horizontal") * speed * Time.deltaTime), 0,
+                                           (Input.GetAxis("Vertical") * speed * Time.deltaTime)));
+
+    }
+}
